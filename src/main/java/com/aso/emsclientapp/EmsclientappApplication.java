35ee17f67f8f6c1bf94b80578bbe5965835b0d5b
Akanimo Sam Okure <akan.okure@gmail.com>
2024-07-15 22:34:56 +0100

    initial setup

---
+++ src/main/java/com/aso/emsclientapp/EmsclientappApplication.java
@@ -0,0 +1,13 @@
+package com.aso.emsclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsclientappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsclientappApplication.class, args);
+	}
+
+}
